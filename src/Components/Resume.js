b86de05889c84e7828703a46c174251e6ca2c641
Rob Quinn <Rob.M.Quinn@gmail.com>
2020-04-05 01:54:52 -0400

    removed unnecessary code

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -16,10 +16,7 @@ class Resume extends Component {
             <p>{work.description}</p>
         </div>
       })
-      var skills = this.props.data.skills.map(function(skills){
-        var className = 'bar-expand '+skills.name.toLowerCase();
-        return <li key={skills.name}><span style={{width:skills.level}}className={className}></span><em>{skills.name}</em></li>
-      })
+    
     }
 
     return (
@@ -53,24 +50,7 @@ class Resume extends Component {
 
 
 
-      <div className="row skill">
-
-         <div className="three columns header-col">
-            <h1><span>Skills</span></h1>
-         </div>
-
-         <div className="nine columns main-col">
-
-            <p>{skillmessage}
-            </p>
-
-				<div className="bars">
-				   <ul className="skills">
-					  {skills}
-					</ul>
-				</div>
-			</div>
-      </div>
+  */}
    </section>
     );
   }
