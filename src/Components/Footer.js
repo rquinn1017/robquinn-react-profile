45879bd7f768fd380eea5ec200ab71f50079d7ac
Rob Quinn <Rob.M.Quinn@gmail.com>
2020-04-05 02:18:53 -0400

    updated readme

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -20,8 +20,7 @@ class Footer extends Component {
 
            <ul className="copyright">
               <li>&copy; Copyright 2020 Rob Quinn</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
-           </ul>
+            </ul>
 
         </div>
         <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
